26a6bfa1a95cb2ce7114e82d49c2751b0b4a2920
vidushi-tiwari <vidushi.tiwari@bigbasket.com>
2020-05-04 19:04:49 +0530

    webpack config added

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+module.exports = {
+  // the output bundle won't be optimized for production but suitable for development
+//   mode: 'development',
+  // the app entry point is /src/index.js
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+  	// the output of the webpack build will be in /dist directory
+    path: path.resolve(__dirname, 'dist1'),
+    // the filename of the JS bundle will be bundle.js
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+      	// for any file with a suffix of js or jsx
+        test: /\.jsx?$/,
+        // ignore transpiling JavaScript from node_modules as it should be that state
+        exclude: /node_modules/,
+        // use the babel-loader for transpiling JavaScript to a suitable format
+        loader: 'babel-loader',
+        options: {
+          // attach the presets to the loader (most projects use .babelrc file instead)
+          presets: ["@babel/preset-env", "@babel/preset-react"]
+        }
+      },
+      {
+				// I haven't used SCSS in the base example, but it's here for you if you
+				// want! If you want to use CSS, you can change this next like's regex to
+				// /\.(css|scss)$/ or even just /\.css$/
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader'],
+			},
+    ]
+  }
+};
