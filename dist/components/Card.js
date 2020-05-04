e43bb1540f8024019bc253be1ca06bce4b8f0981
vidushi-tiwari <vidushi.tiwari@bigbasket.com>
2020-05-04 20:49:14 +0530

    Styled components

--- dist/components/Card.js
+++ dist/components/Card.js
@@ -7,6 +7,8 @@ exports["default"] = void 0;
 
 var _react = _interopRequireDefault(require("react"));
 
+var _styledComponents = _interopRequireDefault(require("styled-components"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
@@ -31,7 +33,140 @@ function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Re
 
 function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
 
-// import '../css/card.css';
+function _templateObject11() {
+  var data = _taggedTemplateLiteral(["\n    margin-top: 22px;\n    width: 120px;\n    height: 24px;\n    border-radius: 64px;\n    font-style: normal;\n    font-weight: 600;\n    font-size: 0.75rem;\n    line-height: 1.5rem;\n    background-color: #fff;\n    text-align: center;\n    float: right;\n    color: #E95F62;\n"]);
+
+  _templateObject11 = function _templateObject11() {
+    return data;
+  };
+
+  return data;
+}
+
+function _templateObject10() {
+  var data = _taggedTemplateLiteral(["\n    font-style: normal;\n    font-weight: 600;\n    font-size: 0.625rem;\n    line-height: 0.875rem;\n    margin-top:9px;\n    color: #000000;\n"]);
+
+  _templateObject10 = function _templateObject10() {
+    return data;
+  };
+
+  return data;
+}
+
+function _templateObject9() {
+  var data = _taggedTemplateLiteral(["\n    height:100%;\n"]);
+
+  _templateObject9 = function _templateObject9() {
+    return data;
+  };
+
+  return data;
+}
+
+function _templateObject8() {
+  var data = _taggedTemplateLiteral(["\n    width: 233px;\n    height: 4px;\n    margin-top:8px;\n    background-color: #fff;\n"]);
+
+  _templateObject8 = function _templateObject8() {
+    return data;
+  };
+
+  return data;
+}
+
+function _templateObject7() {
+  var data = _taggedTemplateLiteral(["\n    font-style: normal;\n    font-weight: normal;\n    font-size: 0.625rem;\n    line-height: 0.875rem;\n    margin-top: 2px;\n    color: #8F8F8F;\n"]);
+
+  _templateObject7 = function _templateObject7() {
+    return data;
+  };
+
+  return data;
+}
+
+function _templateObject6() {
+  var data = _taggedTemplateLiteral(["\n    font-style: normal;\n    font-weight: 600;\n    font-size: 0.625rem;\n    line-height: 0.875rem;\n    margin-top: 10px;\n    color: #4A4A4A;\n"]);
+
+  _templateObject6 = function _templateObject6() {
+    return data;
+  };
+
+  return data;
+}
+
+function _templateObject5() {
+  var data = _taggedTemplateLiteral(["\n    font-style: normal;\n    font-weight: 600;\n    font-size: 0.563rem;\n    line-height: 1.125rem;\n    color: #8F8F8F;\n    margin-top: 2px;\n"]);
+
+  _templateObject5 = function _templateObject5() {
+    return data;
+  };
+
+  return data;
+}
+
+function _templateObject4() {
+  var data = _taggedTemplateLiteral(["\n    font-style: normal;\n    font-weight: normal;\n    font-size: 0.813rem;\n    line-height: 1rem;\n    color: #6B6E7B;\n    margin-top: 10px;\n    white-space: normal;\n    height: 2rem;\n"]);
+
+  _templateObject4 = function _templateObject4() {
+    return data;
+  };
+
+  return data;
+}
+
+function _templateObject3() {
+  var data = _taggedTemplateLiteral(["\n font-size: 1.063rem;\n    font-style: normal;\n    font-weight: bold;\n    line-height: 1.313rem;\n    margin-top: 27px;\n    text-transform: capitalize;\n    color: #000000;\n"]);
+
+  _templateObject3 = function _templateObject3() {
+    return data;
+  };
+
+  return data;
+}
+
+function _templateObject2() {
+  var data = _taggedTemplateLiteral(["\n  height: 20px;\n    width: 230px;\n    margin-left:0px;\n    margin-top: 10px;\n    background-color: blue;\n    color: #fff;\n    font-style: normal;\n    font-weight: 600;\n    font-size: 0.75rem;\n    line-height: 1.25rem;\n    color: #FFFFFF;\n    border-radius: 0px 35px 35px 0px;\n\n    & p {\n       margin:0;\n      margin-left: 55px;\n    }\n"]);
+
+  _templateObject2 = function _templateObject2() {
+    return data;
+  };
+
+  return data;
+}
+
+function _templateObject() {
+  var data = _taggedTemplateLiteral(["\n  border-radius: 16px;\n    display: inline-block;\n    box-sizing: border-box;\n    padding: 20px 30px 20px 30px;\n    margin-left: 13px;\n    background-color: aqua;\n    position: relative;\n    height: 300px;\n    width: 300px;\n"]);
+
+  _templateObject = function _templateObject() {
+    return data;
+  };
+
+  return data;
+}
+
+function _taggedTemplateLiteral(strings, raw) { if (!raw) { raw = strings.slice(0); } return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
+
+var CardBackground = _styledComponents["default"].div(_templateObject());
+
+var CardTag = _styledComponents["default"].div(_templateObject2());
+
+var OfferLabel = _styledComponents["default"].div(_templateObject3());
+
+var OfferDesc = _styledComponents["default"].div(_templateObject4());
+
+var TnC = _styledComponents["default"].div(_templateObject5());
+
+var ConditionLimit = _styledComponents["default"].div(_templateObject6());
+
+var CampaignEndDate = _styledComponents["default"].div(_templateObject7());
+
+var ProgressBar = _styledComponents["default"].div(_templateObject8());
+
+var Progress = _styledComponents["default"].div(_templateObject9());
+
+var GoalAchievement = _styledComponents["default"].div(_templateObject10());
+
+var Cta = _styledComponents["default"].div(_templateObject11());
+
 var Card = /*#__PURE__*/function (_React$Component) {
   _inherits(Card, _React$Component);
 
@@ -46,35 +181,20 @@ var Card = /*#__PURE__*/function (_React$Component) {
   _createClass(Card, [{
     key: "render",
     value: function render() {
-      // return <div className="card" style={{background:this.props.cardColor}}>
-      //             <div className="tag" style={{background:this.props.cardTagColor}}>
-      //               <p>{this.props.cardTag}</p>   
-      //             </div>
-      //             <div className="offerLabel">{this.props.offerLabel}</div>
-      //             <div className="offerDesc">{this.props.offerDesc}</div>
-      //             <div className="tnc">{this.props.tnc}</div>
-      //             <div className="conditionLimit">{this.props.conditionLimit}</div>
-      //             <div className="campaignEndDate">{this.props.campaignEndDate}</div>
-      //             <div className="progressBar">
-      //                 <div className="progress" style={{width:this.props.progress,background:this.props.progressBarColor}}></div>
-      //             </div>
-      //             <div className="goalAchievement">{this.props.goalAchievement}</div>
-      //             <div className="cta">Show products</div>
-      //        </div>
-      return /*#__PURE__*/_react["default"].createElement("div", {
+      return /*#__PURE__*/_react["default"].createElement(CardBackground, {
         style: {
           background: this.props.cardColor
         }
-      }, /*#__PURE__*/_react["default"].createElement("div", {
+      }, /*#__PURE__*/_react["default"].createElement(CardTag, {
         style: {
           background: this.props.cardTagColor
         }
-      }, /*#__PURE__*/_react["default"].createElement("p", null, this.props.cardTag)), /*#__PURE__*/_react["default"].createElement("div", null, this.props.offerLabel), /*#__PURE__*/_react["default"].createElement("div", null, this.props.offerDesc), /*#__PURE__*/_react["default"].createElement("div", null, this.props.tnc), /*#__PURE__*/_react["default"].createElement("div", null, this.props.conditionLimit), /*#__PURE__*/_react["default"].createElement("div", null, this.props.campaignEndDate), /*#__PURE__*/_react["default"].createElement("div", null, /*#__PURE__*/_react["default"].createElement("div", {
+      }, /*#__PURE__*/_react["default"].createElement("p", null, this.props.cardTag)), /*#__PURE__*/_react["default"].createElement(OfferLabel, null, this.props.offerLabel), /*#__PURE__*/_react["default"].createElement(OfferDesc, null, this.props.offerDesc), /*#__PURE__*/_react["default"].createElement(TnC, null, this.props.tnc), /*#__PURE__*/_react["default"].createElement(ConditionLimit, null, this.props.conditionLimit), /*#__PURE__*/_react["default"].createElement(CampaignEndDate, null, this.props.campaignEndDate), /*#__PURE__*/_react["default"].createElement(ProgressBar, null, /*#__PURE__*/_react["default"].createElement(Progress, {
         style: {
           width: this.props.progress,
           background: this.props.progressBarColor
         }
-      })), /*#__PURE__*/_react["default"].createElement("div", null, this.props.goalAchievement), /*#__PURE__*/_react["default"].createElement("div", null, "Show products"));
+      })), /*#__PURE__*/_react["default"].createElement(GoalAchievement, null, this.props.goalAchievement), /*#__PURE__*/_react["default"].createElement(Cta, null, "Show products"));
     }
   }]);
 
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,124 @@
 import React from "react";
 // import '../css/card.css';
+import styled from 'styled-components';
+
+const CardBackground = styled.div`
+  border-radius: 16px;
+    display: inline-block;
+    box-sizing: border-box;
+    padding: 20px 30px 20px 30px;
+    margin-left: 13px;
+    background-color: aqua;
+    position: relative;
+    height: 300px;
+    width: 300px;
+`;
+
+const CardTag = styled.div`
+  height: 20px;
+    width: 230px;
+    margin-left:0px;
+    margin-top: 10px;
+    background-color: blue;
+    color: #fff;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 0.75rem;
+    line-height: 1.25rem;
+    color: #FFFFFF;
+    border-radius: 0px 35px 35px 0px;
+
+    & p {
+       margin:0;
+      margin-left: 55px;
+    }
+`;
+
+const OfferLabel = styled.div`
+ font-size: 1.063rem;
+    font-style: normal;
+    font-weight: bold;
+    line-height: 1.313rem;
+    margin-top: 27px;
+    text-transform: capitalize;
+    color: #000000;
+`;
+
+const OfferDesc = styled.div`
+    font-style: normal;
+    font-weight: normal;
+    font-size: 0.813rem;
+    line-height: 1rem;
+    color: #6B6E7B;
+    margin-top: 10px;
+    white-space: normal;
+    height: 2rem;
+`;
+
+const TnC = styled.div`
+    font-style: normal;
+    font-weight: 600;
+    font-size: 0.563rem;
+    line-height: 1.125rem;
+    color: #8F8F8F;
+    margin-top: 2px;
+`;
+
+
+const ConditionLimit = styled.div`
+    font-style: normal;
+    font-weight: 600;
+    font-size: 0.625rem;
+    line-height: 0.875rem;
+    margin-top: 10px;
+    color: #4A4A4A;
+`;
+
+const CampaignEndDate = styled.div`
+    font-style: normal;
+    font-weight: normal;
+    font-size: 0.625rem;
+    line-height: 0.875rem;
+    margin-top: 2px;
+    color: #8F8F8F;
+`;
+
+const ProgressBar = styled.div`
+    width: 233px;
+    height: 4px;
+    margin-top:8px;
+    background-color: #fff;
+`;
+
+const Progress = styled.div`
+    height:100%;
+`;
+
+const GoalAchievement = styled.div`
+    font-style: normal;
+    font-weight: 600;
+    font-size: 0.625rem;
+    line-height: 0.875rem;
+    margin-top:9px;
+    color: #000000;
+`;
+
+const Cta = styled.div`
+    margin-top: 22px;
+    width: 120px;
+    height: 24px;
+    border-radius: 64px;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 0.75rem;
+    line-height: 1.5rem;
+    background-color: #fff;
+    text-align: center;
+    float: right;
+    color: #E95F62;
+`;
+
+
 
 class Card extends React.Component {
 
@@ -9,37 +128,21 @@ class Card extends React.Component {
    
 
   render () {
-    // return <div className="card" style={{background:this.props.cardColor}}>
-    //             <div className="tag" style={{background:this.props.cardTagColor}}>
-    //               <p>{this.props.cardTag}</p>   
-    //             </div>
-    //             <div className="offerLabel">{this.props.offerLabel}</div>
-    //             <div className="offerDesc">{this.props.offerDesc}</div>
-    //             <div className="tnc">{this.props.tnc}</div>
-    //             <div className="conditionLimit">{this.props.conditionLimit}</div>
-    //             <div className="campaignEndDate">{this.props.campaignEndDate}</div>
-    //             <div className="progressBar">
-    //                 <div className="progress" style={{width:this.props.progress,background:this.props.progressBarColor}}></div>
-    //             </div>
-    //             <div className="goalAchievement">{this.props.goalAchievement}</div>
-    //             <div className="cta">Show products</div>
-    //        </div>
-
-    return <div style={{background:this.props.cardColor}}>
-                <div style={{background:this.props.cardTagColor}}>
+    return <CardBackground style={{background:this.props.cardColor}}>
+                <CardTag style={{background:this.props.cardTagColor}}>
                   <p>{this.props.cardTag}</p>   
-                </div>
-                <div>{this.props.offerLabel}</div>
-                <div>{this.props.offerDesc}</div>
-                <div>{this.props.tnc}</div>
-                <div>{this.props.conditionLimit}</div>
-                <div>{this.props.campaignEndDate}</div>
-                <div>
-                    <div style={{width:this.props.progress,background:this.props.progressBarColor}}></div>
-                </div>
-                <div>{this.props.goalAchievement}</div>
-                <div>Show products</div>
-           </div>
+                </CardTag>
+                <OfferLabel>{this.props.offerLabel}</OfferLabel>
+                <OfferDesc>{this.props.offerDesc}</OfferDesc>
+                <TnC>{this.props.tnc}</TnC>
+                <ConditionLimit>{this.props.conditionLimit}</ConditionLimit>
+                <CampaignEndDate>{this.props.campaignEndDate}</CampaignEndDate>
+                <ProgressBar>
+                    <Progress style={{width:this.props.progress,background:this.props.progressBarColor}}></Progress>
+                </ProgressBar>
+                <GoalAchievement>{this.props.goalAchievement}</GoalAchievement>
+                <Cta>Show products</Cta>
+           </CardBackground>
   }
 }
 
